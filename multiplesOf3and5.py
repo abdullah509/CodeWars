d58872c385b88014ab3392785dae716b9f611ca5
abdullah509 <abdullahashraf509@gmail.com>
2020-05-29 16:05:30 +0100

    My solution to https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-1-multiples-of-3-and-5

---
+++ multiplesOf3and5.py
@@ -0,0 +1,20 @@
+
+#Project Euler: Problem 1: Multiples of 3 and 5
+#If we list all the natural numbers below 10 that
+#are multiples of 3 or 5, we get 3, 5, 6 and 9.
+#The sum of these multiples is 23.
+
+#Find the sum of all the multiples of 3 or 5
+#below the provided parameter value number.
+
+def multiplesOf3and5(number):
+    num = 0
+    answer = 0
+    while num < number:
+        if num % 3 == 0 or num % 5 == 0:
+            answer += num
+
+        num+=1
+    print(answer)
+
+multiplesOf3and5(49) #543
